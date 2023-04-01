67aadbca7bea2c74d03cbf66db2eb5eb0f623087
MateruX <materuxx@gmail.com>
2023-04-01 15:00:48 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Users and Groups List</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User managment System</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="insert_users.js"></script>
 	  <script src="insert_groups.js"></script>
